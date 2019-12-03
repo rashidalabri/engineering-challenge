004eb1d92a1e04982bcedea1021f25a895483abd
rashidalabri <itsrashidalabri@gmail.com>
2019-12-03 02:36:20 -0800

    Create routes for endpoints

--- backend/api.py
+++ backend/api.py
@@ -1,8 +1,24 @@
-from flask import Flask, escape, request
+import json
+
+from flask import Flask, escape, request, jsonify, abort
+from flask_restful import Resource, Api, reqparse
+
+import resources
+
+# TODO: Implement local and server-side caching
+
 
 app = Flask(__name__)
+api = Api(app)
+
+api.add_resource(resources.CharacterList, '/characters')
+api.add_resource(resources.Character, '/characters/<string:id>')
 
 @app.route('/')
-def hello():
-    name = request.args.get("name", "World")
-    return f'Hello, {escape(name)}!'
+def index():
+    return {}
+
+@app.errorhandler(400)
+def resource_does_not_exist(error):
+    response = jsonify({'message': error['message']})
+    return response
