c96bcbd91d06342caa3b221b3b3eec700578bc81
rashidalabri <itsrashidalabri@gmail.com>
2019-12-03 02:33:30 -0800

    Add config file

---
+++ backend/config.py
@@ -0,0 +1,5 @@
+DATA_CHARACTERS_FILE_DIR = '../data/harry-potter-characters.json'
+DATA_HOUSES_FILE_DIR = '../data/harry-potter-houses.json'
+DATA_SPELLS_FILE_DIR = '../data/harry-potter-spells.json'
+
+PAGINATION_MAX_ELEMS_PER_PAGE = 50
